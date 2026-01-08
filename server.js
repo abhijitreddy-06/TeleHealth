47aaf085d6338097ace0cef2ae791058da5cb124
abhijitreddy-06 <abhijitreddy828@gmail.com>
2026-01-08 15:19:21 +0530

    updated

--- server.js
+++ server.js
@@ -157,7 +157,6 @@ const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', 
     sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', 
-    maxAge: 7 * 24 * 60 * 60 * 1000, 
     path: '/'
 };
 
